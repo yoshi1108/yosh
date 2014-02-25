a4886f143539f3b78ea49cf1a22da324dd5ed259
yoshi1108 <yoshi1108@gmail.com>
2014-02-26 00:54:04 +0900

    'hoge'

--- src/scala/HelloWorld.scala
+++ src/scala/HelloWorld.scala
@@ -5,18 +5,22 @@ breakable{
 //System.setProperty("http.proxyHost","proxygate2.nic.nec.co.jp")
 //System.setProperty("http.proxyPort","8080")
 
-println("--- from file--------------------------");
-for (line <- Source.fromFile("/oracle/home/memo.txt").getLines) {
-	if ( line.indexOf("する") != -1 ) {
-  		println(line)
+def func(lines: Iterator[String]): Unit = {
+    for (line <- lines ) {
+    	if (line.indexOf("する") != -1 ) {
+      		println(line)
+		}
 	}
 }
+
+println("--- from file--------------------------");
+
+var lines = Source.fromFile("/oracle/home/memo.txt").getLines;
+func(lines);
 
 println("--- from web--------------------------");
-for (line <- Source.fromURL("http://www.mwsoft.jp/programming/scala/fileread.html").getLines) {
-	if ( line.indexOf("link") != -1 ) {
-  		println(line)
-	}
-}
+lazy val webLines = Source.fromURL("http://www.mwsoft.jp/programming/scala/fileread.html").getLines;
 
-}
+func(webLines);
+
+}	
