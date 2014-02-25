9850112c125d88066e89c34981be5bb3dafb5244
yoshi1108 <yoshi1108@gmail.com>
2014-02-25 10:43:02 +0900

    'hoge'

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 
 class Test{
-	HashMap<String,String> hogeMap = new HashMap<String,String>();
+	static HashMap<String,String> hogeMap = new HashMap<String,String>();
 	private static void hoge(){
 		HashMap<String,String> map = new HashMap<String,String>();
 		map.put("hoge1", "1");
@@ -10,7 +10,7 @@ class Test{
 		for (String key: map.keySet()) {
 			System.out.println(key + "," + map.get(key));	
 		}
-		hogeMap.put("hoge");
+		hogeMap.put("hoge","hoge");
 	}
 	public static void main(String args[]){
 		System.out.println("hoge\n");
