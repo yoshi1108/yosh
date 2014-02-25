4ddd92d4670a4d589858ee2f11366aa572cc05fa
yoshi1108 <yoshi1108@gmail.com>
2014-02-25 18:56:08 +0900

    'hoge'

---
+++ src/scala/HelloWorld.scala
@@ -0,0 +1,23 @@
+import scala.io.Source
+import scala.util.control.Breaks.{break,breakable} 
+breakable{
+
+System.setProperty("http.proxyHost","proxygate2.nic.nec.co.jp")
+System.setProperty("http.proxyPort","8080")
+
+println("--- from file--------------------------");
+for (line <- Source.fromFile("/oracle/home/memo.txt").getLines) {
+	if ( line.indexOf("する") != -1 ) {
+		continue
+  		println(line)
+	}
+}
+
+println("--- from web--------------------------");
+for (line <- Source.fromURL("http://www.mwsoft.jp/programming/scala/fileread.html").getLines) {
+	if ( line.indexOf("link") != -1 ) {
+  		println(line)
+	}
+}
+
+}
